dc977bec26a43efb149d6fd7ece60a0280448171
pmsiew <siewpengmun@yahoo.com>
2025-05-18 00:06:44 +0200

    Remove button

--- src/routes/NameSearch/NameSearch.tsx
+++ src/routes/NameSearch/NameSearch.tsx
@@ -93,13 +93,13 @@ function NameSearch() {
           placeholder="Name"
         />
         <button type="submit" className="name-submit is-size-5 xs-x-self-center">Search</button>
-        <button
+        {/* <button
           type="button"
           className="random is-size-8"
           onClick={(event) => onRandomTagClick(event, hooks, service)}
         >
           {'I\'m Feeling Lucky'}
-        </button>
+        </button> */}
 
       </form>
       <hr />
